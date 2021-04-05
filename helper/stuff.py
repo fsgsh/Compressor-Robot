80778f5b7a1d3f2692c39e19e1b0bb33bd91f03e
Mr. SAFONE <77989182+AsmSafone@users.noreply.github.com>
2021-04-05 21:20:41 +0600

    Update stuff.py

--- helper/stuff.py
+++ helper/stuff.py
@@ -29,7 +29,7 @@ async def up(event):
 async def start(event):
     ok = await event.client(GetFullUserRequest(event.sender_id))
     await event.reply(
-        f"👋🏻 **Hi `{ok.user.first_name}` !**\n\nI'm **HEVC Video Compressor**🔥\n__Which Can Encode & Compress Videos. Reduce Size of Videos With Negligible Quality Change! You Can Generate Sample Videos / Screenshots Too...🤗__",
+        f"👋🏻 **Hi `{ok.user.first_name}` !**\n\nI'm **HEVC Video Compressor**🔥\n__Which Can Encode & Compress Videos. Reduce Size of Videos With Negligible Quality Change! You Can Generate Sample Videos / Screenshots Too 🤗__",
         buttons=[
             [Button.inline("⚙️ HELP ⚙️", data="ihelp")],
             [
@@ -56,7 +56,7 @@ async def ihelp(event):
 async def beck(event):
     ok = await event.client(GetFullUserRequest(event.sender_id))
     await event.edit(
-        f"👋🏻 **Hi `{ok.user.first_name}` !**\n\nI'm **HEVC Video Compressor**🔥\n__Which Can Encode & Compress Videos. Reduce Size of Videos With Negligible Quality Change! You Can Generate Sample Videos / Screenshots Too...🤗__",
+        f"👋🏻 **Hi `{ok.user.first_name}` !**\n\nI'm **HEVC Video Compressor**🔥\n__Which Can Encode & Compress Videos. Reduce Size of Videos With Negligible Quality Change! You Can Generate Sample Videos / Screenshots Too 🤗__",
         buttons=[
             [Button.inline("⚙️ HELP ⚙️", data="ihelp")],
             [
